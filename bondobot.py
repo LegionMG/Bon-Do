4ea81a2bd574820164327e578408090daa2de217
Daniil.Gasnikov <legionlon@mail.ru>
2019-03-06 13:43:05 +0500

    added locales

--- bondobot.py
+++ bondobot.py
@@ -7,28 +7,14 @@ from telegram.error import BadRequest
 from tinydb import TinyDB, Query
 from tinydb_serialization import SerializationMiddleware, Serializer
 from datetime import date
+from locales import Conf
 import telegram.constants as constants
 import random, re, time, datetime, operator, logging, os
 
-RANTS_ONE = ['Притворяюсь, что считать пидоров это долго', 
-                'Нагибаемся, ягодицы раздвигаем', 
-                'Так блядь, ну че тут у вас',
-                "Мmmmm... Hmmmmmmmm.... Oh! Forgive me, I didn't notice you, I was absorbed in thought"
-                ]
-RANTS_TWO = ['Так, падажжи ебана', 
-                'Ага, дрочете',
-                'Два обзора до конца месяца',
-                'Ну такое',
-                'Нет бы крючки сачки нажимать',
-                '''
-```
-try { 
-    Thread.sleep(1500);
-} catch (InterruptedException e) { 
-    log.error("Ты ебанушка, ты че сделал? Ты Серому фотоаппарат прострелил", e);
-}
-```
-''']
+LANGUAGE = 'ru'
+RANTS_ONE = Conf.loc[LANGUAGE]["rants"]["one"]
+RANTS_TWO = Conf.loc[LANGUAGE]["rants"]["two"]
+BAD_STAT = {ChatMember.LEFT, ChatMember.KICKED, ChatMember.RESTRICTED}
 
 class DateSerializer(Serializer):
     OBJ_CLASS = date
@@ -38,6 +24,7 @@ class DateSerializer(Serializer):
 
     def decode(self, s):
         return datetime.datetime.strptime(s, '%Y-%m-%d').date()
+
 
 TOKEN = os.environ['BONDOBOT_TOKEN']
 
@@ -54,7 +41,7 @@ db = TinyDB('db.json', storage=serialization)
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="оохххх...")
+    bot.send_message(chat_id=update.message.chat_id, text=Conf.loc[LANGUAGE]["start"])
 
 
 def check_for_ohs_and_pidors(bot, update):
@@ -63,6 +50,7 @@ def check_for_ohs_and_pidors(bot, update):
     if chat_type == Chat.GROUP or chat_type == Chat.SUPERGROUP:
         save_pidor(bot=bot, update=update)
 
+
 def save_pidor(bot, update):
     Pidor = Query()
     user_id = update.message.from_user.id
@@ -70,12 +58,14 @@ def save_pidor(bot, update):
     if not db.contains((Pidor.user_id == user_id) & (Pidor.chat_id == chat_id)):
         db.insert({'user_id': user_id, 'chat_id': chat_id, 'won': 0})
 
+
 def reply_to_oh(bot, update):
     this_message = update.message
     text = this_message.text
 
     if pattern.search(text):
         bot.send_message(update.message.chat.id, random.randint(1, 8) * 'о' + random.randint(1, 8) * 'х' + random.randint(0, 3) * '.')
+
 
 def pidor(bot, update):
     chat_id = update.message.chat.id
@@ -85,7 +75,6 @@ def pidor(bot, update):
     if not saved_pidor:
         saved_pidor = {'date': date(1970, 1, 1), 'chat_id': chat_id, 'user_id': 0}
     saved = saved_pidor['date']
-    
     text = ''
     pidor_user = None
 
@@ -96,19 +85,18 @@ def pidor(bot, update):
         while not pidor_of_the_day:
             pidors_length = len(pidors)
             if pidors_length == 0:
-                bot.send_message(chat_id=chat_id, text='Чет нихуя не нашел')
+                bot.send_message(chat_id=chat_id, text=Conf.loc[LANGUAGE]["not_found"])
                 return
             index = random.randint(0, pidors_length-1)
             pidor_of_the_day = pidors[index]
             pidor_user = get_pidor_user(bot=bot, chat_id=chat_id, user_id=pidor_of_the_day['user_id'])
             if not pidor_user:
-                return
-            
-            if pidor_user.status == ChatMember.LEFT or pidor_user.status == ChatMember.KICKED or pidor_user.status == ChatMember.RESTRICTED:
+                return  
+            if pidor_user.status in BAD_STAT:
                 pidor_user = pidor_user.user
-                bot.send_message(chat_id=chat_id, text='Щас бы победил ' + pidor_user.mention_html(pidor_user.full_name) + ', но телега сообщает, шо он съебал',
+                bot.send_message(chat_id=chat_id, text=Conf.loc[LANGUAGE]["bad_status"].format(user=pidor_user.mention_html(pidor_user.full_name)),
                         parse_mode=ParseMode.HTML)
-                bot.send_message(chat_id=chat_id, text='Дубль два')
+                bot.send_message(chat_id=chat_id, text=Conf.loc[LANGUAGE]["try_again"])
                 db.remove((Pidor.won.exists()) & (Pidor.chat_id == chat_id) & (Pidor.user_id == pidor_user.id))
                 del pidors[index]
                 pidor_of_the_day = None
@@ -118,8 +106,8 @@ def pidor(bot, update):
                 saved_pidor['user_id'] = pidor_of_the_day['user_id']
                 saved_pidor['date'] = today
                 db.upsert(saved_pidor, (Pidor.date.exists()) & (Pidor.chat_id == chat_id))
-                text = 'Ну кароч, пидор дня - '
-                bot.send_message(chat_id=chat_id, text='Щяс будем определять пидора дня')
+                text = Conf.loc[LANGUAGE]["found"]
+                bot.send_message(chat_id=chat_id, text=Conf.loc[LANGUAGE]["search"])
                 time.sleep(1.5)
                 bot.send_message(chat_id=chat_id, text=random.choice(RANTS_ONE))
                 time.sleep(1.5)
@@ -129,16 +117,18 @@ def pidor(bot, update):
         pidor_user = get_pidor_user(bot=bot, chat_id=chat_id, user_id=saved_pidor['user_id'])
         if not pidor_user:
             return
-        text = 'Меня сегодня уже пробуждали, пидор дня - '
+        text = Conf.loc[LANGUAGE]["already_found"]
     pidor_user = pidor_user.user
     bot.send_message(chat_id=chat_id, text=text + pidor_user.mention_html(pidor_user.full_name), parse_mode=ParseMode.HTML)
-        
+
+
 def get_pidor_user(bot, chat_id, user_id):
     try:
         return bot.get_chat_member(chat_id=chat_id, user_id=user_id)
     except BadRequest:
-        bot.send_message(chat_id=chat_id, text='Чет говно какое-то, казах блядь. User_id: ' + str(user_id))
+        bot.send_message(chat_id=chat_id, text=Conf.loc[LANGUAGE]["error_by"].format(user=user_id))
         return None
+
 
 def pidor_stats(bot, update):
     chat_id = update.message.chat.id
@@ -146,9 +136,9 @@ def pidor_stats(bot, update):
     Pidor = Query()
     pidors = db.search((Pidor.won.exists()) & (Pidor.chat_id == chat_id))
     if len(pidors) == 0:
-        message = 'Нихуя нету, все пидоры'
+        message = Conf.loc[LANGUAGE]["empty_list"]
     else:
-        message = '*Топ пидоров*:\n\n'
+        message = Conf.loc[LANGUAGE]["top"]
         sorted_pidors = sorted(pidors, key=operator.itemgetter('won'), reverse=True)
         for pidor in sorted_pidors:
             if pidor['won'] == 0:
